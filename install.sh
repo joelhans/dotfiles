d7688e053b7d755a62139c793d028e90413c592a
Joel Hans <joel.g.hans@gmail.com>
2018-02-06 20:06:00 -0700

    Converted symlinks over to current dir.

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# -------------------------------------
+# Get current dir
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # .gitconfig and .gitignore_global
-# -------------------------------------
-ln -sfv ~/.dotfiles/.gitconfig ~
-ln -sfv ~/.dotfiles/.gitignore_global ~
 
-# -------------------------------------
+ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
+
 # Hyper.js
-# -------------------------------------
-ls -sfv ~/.dotfiles/.hyper.js ~
 
-# -------------------------------------
+ls -sfv "$DOTFILES_DIR/.hyper.js" ~
+
 # ZSH
-# -------------------------------------
 
+ls -sfv "$DOTFILES_DIR/.zshrc" ~
 
 
 # Install the ZSH syntax highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
