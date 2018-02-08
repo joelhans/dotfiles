04a6dbd60c123e9acb8f6f59b0f45e8aa9c7b2b0
Joel Hans <joel.g.hans@gmail.com>
2018-02-08 11:16:24 -0700

    Fixed links and added downloading for theme.

--- install.sh
+++ install.sh
@@ -17,17 +17,71 @@ ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
 # Hyper.js
 #
 
-ls -sfv "$DOTFILES_DIR/.hyper.js" ~
+ln -sfv "$DOTFILES_DIR/.hyper.js" ~
 
 
 #
 # ZSH
 #
 
-ls -sfv "$DOTFILES_DIR/.zshrc" ~
+# install_zsh () {
+#   # Test to see if zshell is installed.  If it is:
+#   if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+#   	echo "ZSH is installed"
+#     # Install Oh My Zsh if it isn't already present
+#     if [[ ! -d ~/.oh-my-zsh/ ]]; then
+#       sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#     fi
+#     # Set the default shell to zsh if it isn't currently set to zsh
+#     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+#       chsh -s $(which zsh)
+#     fi
+#   else
+#     # If zsh isn't installed, get the platform of the current machine
+#     platform=$(uname);
+#     echo "ZSH is not installed"
+#     # If the platform is Linux, try an apt-get to install zsh and then recurse
+#     # if [[ $platform == 'Linux' ]]; then
+#     #   if [[ -f /etc/redhat-release ]]; then
+#     #     sudo dnf install zsh
+#     #     install_zsh
+#     #   fi
+#     #   if [[ -f /etc/debian_version ]]; then
+#     #     sudo apt-get install zsh
+#     #     install_zsh
+#     #   fi
+#     # # If the platform is OS X, tell the user to install zsh :)
+#     # elif [[ $platform == 'Darwin' ]]; then
+#     #   echo "We'll install zsh, then re-run this script!"
+#     #   brew install zsh
+#     #   exit
+#     # fi
+#   fi
+# }
 
-# Install the ZSH syntax highlighting plugin
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Run the zsh_install function
+
+# install_zsh
+
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+
+# Install the ZSH spaceship theme if not already installed
+if [[ ! -d $HOME/.oh-my-zsh/custom/themes/spaceship-prompt ]]; then
+	git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+	ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
+
+# Install the ZSH syntax highlighting plugin if it's not already installed
+if [[ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+# Symlink .zshrc
+
+ln -sfv "$DOTFILES_DIR/.zshrc" ~
+
+# Reload zsh settings
+source ~/.zshrc
 
 
 #
@@ -37,9 +91,9 @@ ls -sfv "$DOTFILES_DIR/.zshrc" ~
 # To create a list of current packages, use the following command:
 # apm list --bare --installed --dev false > packages.txt
 
-ls -sfv "$DOTFILES_DIR/atom/config.cson" ~/.atom/
-ls -sfv "$DOTFILES_DIR/atom/styles.less" ~/.atom/
+ln -sfv "$DOTFILES_DIR/atom/config.cson" ~/.atom/
+ln -sfv "$DOTFILES_DIR/atom/styles.less" ~/.atom/
 
 # Install packages based on packages.txt
 
-apm install --packages-file packages.txt
+# apm install --packages-file $DOTFILES_DIR/atom/packages.txt
