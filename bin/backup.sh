f751c75b10c8101f957038122b75a6fba9edef20
Joel Hans <joel.g.hans@gmail.com>
2020-07-20 15:43:46 -0700

    Updates

--- bin/backup.sh
+++ bin/backup.sh
@@ -15,25 +15,25 @@ rsync -azh --no-i-r --info=progress2 --delete \
 echo "Done backing up photographs to external drive."
 
 # Home directory
-# echo "Backing up home directory."
-# echo "Backing up home directory to external drive."
-# rsync -azh --no-i-r --info=progress2 \
-#     --include ".ssh/" \
-#     --exclude ".*" \
-#     --exclude "Downloads/" \
-#     --exclude "photography/" \
-#     --exclude "node_modules/" \
-#     --exclude "netdata/" \
-#     /home/$USER/ $PI_USER@$PI_URL:$EXTDIR/home
-# echo "Done backing up home directory to external drive."
+echo "Backing up home directory."
+echo "Backing up home directory to external drive."
+rsync -azh --no-i-r --info=progress2 \
+    --include ".ssh/" \
+    --exclude ".*" \
+    --exclude "Downloads/" \
+    --exclude "photography/" \
+    --exclude "node_modules/" \
+    --exclude "bak-of-bak" \
+    /home/$USER/ $PI_USER@$PI_URL:$EXTDIR/home
+echo "Done backing up home directory to external drive."
 
 # Git
-# echo "Backing up git directories to both drives."
-# for i in "${GITDIRS[@]}"; do :
-#     cd $i
-#     mkdir tmp
-#     git bundle create tmp/${PWD##*/} --all
-#     rsync tmp/${PWD##*/} $PI_USER@$PI_URL:$EXTDIR/git
-#     rm -rf tmp
-# done
-# echo "Done backing up git directories to external drive." 
+echo "Backing up git directories to both drives."
+for i in "${GITDIRS[@]}"; do :
+    cd $i
+    mkdir tmp
+    git bundle create tmp/${PWD##*/} --all
+    rsync tmp/${PWD##*/} $PI_USER@$PI_URL:$EXTDIR/git
+    rm -rf tmp
+done
+echo "Done backing up git directories to external drive." 
