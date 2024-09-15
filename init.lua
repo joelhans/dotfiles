df2d4ee939851770245270a5d3b7f545951b0cd7
Joel Hans <j@joelhans.com>
2024-09-15 11:20:56 -0700

    New neovim things

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -68,3 +69,19 @@ config.setup({
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+-- Functions to always open nvim-tree on nvim open, and close when there are no
+-- more buffers.
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
