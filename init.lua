5283671fdceb541a039b21220e9c712173d164b4
Joel Hans <j@joelhans.com>
2025-02-21 05:42:02 -0700

    Get copy working again

--- init.lua
+++ init.lua
@@ -35,8 +35,7 @@ vim.keymap.set('n', '<Leader>sv', ':source $MYVIMRC<CR>')
 -- Unmap space in visual mode
 vim.keymap.set('v', ' ', '')
 -- Copy to system clipboard
-vim.keymap.set('n', '<Leader>cc', '"*y')
--- vnoremap <C-c> "*y
+vim.keymap.set({ "n", "v" }, "<leader>c", [["+y]])
 
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
@@ -88,11 +87,3 @@ local function open_nvim_tree()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
-    end
-  end
-})
